e398d1711fcb3d74072cbc14520cf71e6b775bae
208486 <208486@KB37-1-120-C03>
2024-04-17 16:33:52 +0300

    Добавьте файлы проекта.

---
+++ PM_02_ArturKazarian_4ISIP520/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PM_02_ArturKazarian_4ISIP520
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
